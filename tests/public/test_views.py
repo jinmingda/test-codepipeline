242d096dd63687b0584e31954e8e7d93a704f252
Mingda Jin <jinmingda77@gmail.com>
2020-02-13 11:21:04 -0800

    Style change in compliance with black style

--- tests/public/test_views.py
+++ tests/public/test_views.py
@@ -6,6 +6,6 @@ class TestViews:
 
     def test_home(self, testapp):
         """Test homepage."""
-        response = testapp.get('/')
+        response = testapp.get("/")
         assert response.status_code == 200
         assert response.text == "HelloWorld"
